c5b1bd0e7ecb04466ad6785b74ab572d13d2fda8
Marcos <juangura@gmail.com>
2025-01-21 23:47:33 -0400

    DEV: Add controller to manage individual addition of search terms

---
+++ app/controllers/saved_search_extension_controller.rb
@@ -0,0 +1,26 @@
+class SavedSearchExtensionController < ApplicationController
+  requires_plugin "discourse-saved-searches"
+  before_action :ensure_logged_in
+
+  def add_element
+    raise Discourse::InvalidParameters.new(:missing_username) unless params[:username].present?
+    raise Discourse::InvalidParameters.new(:missing_search_term) unless params[:search_term].present?
+    user = User.find_by(username: params[:username])
+    raise Discourse::NotFound unless user
+    guardian.ensure_can_edit!(user)
+    user.guardian.ensure_can_use_saved_searches!
+    searches_stored = user.saved_searches
+
+    if searches_stored.count >= SiteSetting.max_saved_searches
+      raise Discourse::InvalidParameters.new(:limit_reached)
+    end
+
+    query = params[:search_term].strip
+    frequency = SavedSearch.frequencies[:daily]
+    SavedSearch.transaction do
+      saved_search = user.saved_searches.find_or_initialize_by(query: query)
+      saved_search.frequency ||= frequency
+      saved_search.save!
+    end
+  end
+end
